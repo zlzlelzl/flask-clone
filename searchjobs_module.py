de30e7259a6bacc210402138f1abac18e9f89617
zlzlelzl <wkdwl1234@naver.com>
2021-09-23 09:58:25 +0900

    1

--- searchjobs_module.py
+++ searchjobs_module.py
@@ -9,6 +9,8 @@ celery = Celery("task", BROKER_URL="redis://localhost:6379",
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
 
+searchjobs.delay(term).status
+
 
 @celery.task()
 def searchjobs(term):

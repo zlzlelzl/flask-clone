c444663aaa4a75f037f4263ecfc0521fd848449e
zlzlelzl <wkdwl1234@naver.com>
2022-03-15 16:58:19 +0900

    implement LRU cache

---
+++ lru_cache.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self,key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+class Cache:
+    def __init__(self, max_cache_size:int = 2):
+        self.head = None
+        self.tail = None
+        self.size = 0
+        self.max_cache_size = max_cache_size
+        self.cache = {}
+
+    def put(self, key, value) -> bool:
+        # 초기 세팅
+        if self.head is None:
+            self.head = Node(None, None)
+            self.tail = Node(None, None)
+            self.head.next = self.tail
+            self.tail.prev = self.head
+                
+        # 헤드에 연결하여 우선순위를 높일 temp 포인터 선언
+        if self.cache.get(key) is None:
+            temp = Node(key,value)
+            self.cache[key] = temp
+            self.size += 1
+            
+        # 기존 키라면 연결을 끊어줌
+        else:
+            temp = self.cache[key]
+            temp.prev.next = temp.next
+            temp.next.prev = temp.prev
+        
+        # 초기 세팅에서 head와 tail을 연결시켜 줬기 때문에 tail은 계속 뒤로 미뤄짐
+        self.head.next.prev = temp
+        temp.next = self.head.next
+        
+        self.head.next = temp
+        temp.prev = self.head
+        
+        # maxsize를 넘어가면 삭제
+        if self.size > self.max_cache_size:
+            key = self.tail.prev.key
+            
+            self.tail.prev.prev.next = self.tail
+            self.tail.prev = self.tail.prev.prev
+            del self.cache[key]
+              
+        return True
+    
+    def cache_list(self) -> bool:
+        print(self.cache)
+        return True
+    
+if __name__ == "__main__":
+    cache = Cache(4)
+    
+    cache.put("foo",["baa"])
+    cache.put("foo2",["baa2"])
+    cache.put("foo",["baa"])
+    cache.put("foo3",["baa3"])
+    cache.put("foo4",["baa3"])
+    cache.put("foo5",["baa3"])
+    cache.put("foo6",["baa3"])
+
+    cache.cache_list()
--- main.py
+++ main.py
@@ -6,33 +6,37 @@ import os
 from os.path import dirname, join
 from db import db_connect
 import json
+# from lru_cache import Cache, Node
 
 app = Flask(__name__)
 
-conn = db_connect()
+    
+# conn = db_connect()
+
+# lru 테스트를 위한 캐시 제한
+max_cache_size = 2
 
 cache = {}
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 @app.route("/search")
 def search():
     term = request.args.get("term")
 
-    try:
-        cache[term] = json.loads(conn.get(term))
-    except:
-        cache[term] = {}
+    # try:
+    #     cache[term] = json.loads(conn.get(term))
+    # except:
+    #     cache[term] = {}
 
     if not cache.get(term):
-        pass
-        # cache[term] = searchjobs(term)
+        cache[term] = searchjobs(term)
         # conn.set(term, json.dumps(searchjobs(term)))
-
+    
+    print(cache[term])
+ 
     return render_template("search.html", term=term, len_term=len(cache[term]), results=cache[term])
 
 
@@ -43,10 +47,9 @@ def export():
         if not term:
             raise Exception()
         term = term.lower()
-
         if not cache.get(term):
             raise Exception()
-
+        
         write_file(term, cache[term])
 
     except:
